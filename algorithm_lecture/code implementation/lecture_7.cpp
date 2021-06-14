27acfd9a2671561038dcf4c008de87262a0634a1
jihyo <dua.bori@gmail.com>
2021-06-15 04:42:34 +0900

    lecture_7

--- algorithm_lecture/code implementation/lecture_7.cpp
+++ algorithm_lecture/code implementation/lecture_7.cpp
@@ -1,19 +1,59 @@
-#include<stdio.h>
+/*#include <iostream>
+#include <stdio.h>
+
 using namespace std;
-int main() {
+
+int main()
+{
+	char input[101];
 	
-	char a[101], b[101];
-	int i, p = 0;
-	gets(a);
-	for (i = 0; a[i] != '\0'; i++) {
-		if (a[i] != ' ') {
-			if (a[i] >= 65 && a[i] <= 90) {
-				b[p++] = a[i] + 32;
+	gets_s(input);
+
+	for (int i = 0; input[i] != '\0'; i++)
+	{
+		if (input[i] == ' ')
+			continue;
+		if (input[i] >= 65 && input[i] <= 90)
+			cout << static_cast<char>(input[i] + 32);
+		else if (input[i] >= 97 && input[i] <= 122)
+			cout << static_cast<char>(input[i]);
+	}
+	return 0;
+}
+
+
+*/
+#include <iostream>
+#include <stdio.h>
+#include <cstdio>
+#define _CRT_SECURE_NO_WARNINGS
+
+using namespace std;
+
+int main()
+{
+	char input[101] = { 0 , };
+	char output[101] = { 0 , };
+	int j = 0;
+
+	cin.getline(input, 101, '\n');
+
+	for (int i = 0; input[i] != '\0'; i++)
+	{
+		if (input[i] != ' ')
+		{
+			if (input[i] >= 'A' && input[i] <= 'Z')
+			{
+				output[j++] == input[i] + 32;
 			}
-			else b[p++] = a[i];
+			else if (c[i] >= 97 && c[i] <= 122)
+			{
+				output[j++] == input[i];
+			}
 		}
 	}
-	b[p] = '\0';
-	printf("%s\n", b);
+	output[j] = '\0';
+	printf("%s", output);
+
 	return 0;
 }
