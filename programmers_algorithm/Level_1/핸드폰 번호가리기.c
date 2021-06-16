7e0ee12f78e6c56faa11769464a8b59770fd4be3
jihyo <dua.bori@gmail.com>
2021-06-16 23:26:55 +0900

    level_1

--- programmers_algorithm/Level_1/핸드폰 번호가리기.c
+++ programmers_algorithm/Level_1/핸드폰 번호가리기.c
@@ -2,13 +2,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-
 char* solution(const char* phone_number) {
 
     int len = strlen(phone_number);
     char* answer = (char*)malloc(20);
 
     strcpy(answer, phone_number);
+
     for (int i = 0; i < len - 4; i++)
     {
         answer[i] = '*';
