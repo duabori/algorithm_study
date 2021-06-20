639817267f38e47c2edc1ca524004772863e99a7
jihyo <dua.bori@gmail.com>
2021-06-20 23:22:26 +0900

    level_1

---
+++ programmers_algorithm/Level_1/콜라츠 추측.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num) {
+    int answer = 0;
+
+    for (int i = 0; i < 500; i++)
+    {
+        if (num == 1) return i;
+
+        if (num % 2 == 0) num /= 2;
+
+        else if (num % 2 == 1) num = num * 3 + 1;
+
+    }
+    return -1;
+}
